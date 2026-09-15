44ce7ae8bcc5c52d492d9d865ef9e3c6e32e4d52
jonathan-leseux <jonathan.l.costa6@aluno.senai.br>
2026-09-15 17:04:41 -0300

    TaskService iniciada

---
+++ src/main/java/api_teste/ds/services/TaskService.java
@@ -0,0 +1,26 @@
+package api_teste.ds.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import api_teste.ds.models.Task;
+import api_teste.ds.models.User;
+import api_teste.ds.repositories.TaskRepository;
+
+@Service 
+public class TaskService {
+
+    @Autowired 
+    private TaskRepository taskrepository;
+    @Autowired 
+    private UserService userservice;
+
+    
+    
+}
+
